f62cdbd1cca692eef3c00a6fe8b59dfe10a3d197
md <mohammad_daneshamooz@yahoo.com>
2021-04-09 16:58:13 +0430

    optimize version of parallelism

--- main.cpp
+++ main.cpp
@@ -5,15 +5,16 @@
 #include <iostream>
 #include <omp.h>
 #include <chrono>
+#include <vector>
+#include<iomanip>
 #include "public-lib.h"
-
 using namespace std;
 
-const int n = 2000;
-const int p = 10;
+const int n = 1000;
+const int p = 16;
 const double epsilon = 0.0001;
-double params[] = {100, 2, 4, 0.5, 1, 4, 3, 2, 1, 2};
-int attrbite_range[] = {10, 100, 50000, 5, 10, 3, 50, 20, 50, 100};
+double params[] = {100, 2, 4, 0.5, 1, 4, 3, 2, 1, 2,-2,5,7,14,23,37};
+int attrbite_range[] = {10, 100, 50000, 5, 10, 3, 50, 20, 50, 100,10,10,10,20,30,50};
 double norm[p];
 double train_x[n][p] = {0}, train_y[n] = {0};
 double theta[p];
@@ -22,37 +23,69 @@ void set_train_set();
 void tink(double *theta);
 void tink0(double *theta);
 void tink2(double *theta,int thread_num);
+void tink3(double *theta,int thread_num);
 double cal_y(double *x);
 double predict_y(double *x);
 
 int main(int argc, const char **argv) {
-
-  // omp_set_num_threads(NUM_THREADS);
+  vector<vector<double>> times;
+  vector<string> functions;
+  vector<double> temp;
   srand(time(NULL));
   set_train_set();
 
   setcolor(1);
 
   system_clock::time_point start, end;
-
+  functions.push_back("Sequense first implement (tink0 function)");
   start = system_clock::now();
   tink0(theta);
   end = system_clock::now();
-  cout << "Sequense (tink function) single thread time : " << diffclock(end, start)/1000.0
-       << " s\n";
+  temp.push_back(diffclock(end, start)/1000.0);
+  times.push_back(temp);
+  setcolor(1);
+  cout<<functions.back()<<endl;
+  print_vector("theta", theta, p, 2, 0);
+  cout<<endl;
 
+  functions.push_back("Parallel seconad implement (tink2 function)");
+  temp.clear();
   for(int i=1;i<=8;i++){
     start = system_clock::now();
     tink2(theta,i);
     end = system_clock::now();
-    cout << "parallel (tink2 function) thread_num : "<< i <<" time : " << diffclock(end, start)/1000.0
-        << " s\n";
-
+    temp.push_back(diffclock(end, start)/1000.0);
   }
-
-  setcolor(7);
+  times.push_back(temp);
+  setcolor(1);
+  cout<<functions.back()<<endl;
   print_vector("theta", theta, p, 2, 0);
-  cout << "\n";
+  cout<<endl;
+
+  functions.push_back("Parallel third implement (tink3 function)");
+  temp.clear();
+  for(int i=1;i<=8;i++){
+    start = system_clock::now();
+    tink3(theta,i);
+    end = system_clock::now();
+    temp.push_back(diffclock(end, start)/1000.0);
+  }
+  times.push_back(temp);
+  setcolor(1);
+  cout<<functions.back()<<endl;
+  print_vector("theta", theta, p, 2, 0);
+  cout<<endl;
+
+  cout<<setw(45)<<left<<"Implements/Number of threads";
+  for(int i=1;i<=8;i++) cout<<setw(7)<<right<<i;
+  cout<<endl;
+  for(int i=0;i<functions.size();i++){
+    cout<< setw(45)<< left << functions[i];
+    for(auto t : times[i]){
+      cout<<setw(7)<<right<<t;
+    }
+    cout<<endl;
+  }
   // double x[p];
   // x[0] = 1;
   // for (int i = 1; i < p; i++)
@@ -156,35 +189,27 @@ void tink0(double *theta) {
   do {
     last_error = error;
     int j;
+    double error2 = 0;
+
     for (int i = 0; i < n; i++) {
       y_pred[i] = 0;
       for (j = 0; j < p; j++) {
         y_pred[i] += theta[j] * train_x[i][j];
       }
-    }
-
-    double error2 = 0;
-
-    for (int i = 0; i < p; i++) {
-      delta[i] = 0;
-    }
-
-    for (int i = 0; i < n; i++) {
       err_0[i] = y_pred[i] - train_y[i];
       error2 += err_0[i] * err_0[i];
     }
 
     for (int j = 0; j < p; j++){
+      double temp=0;
       for(int i=0;i<n;i++)
-        delta[j] += train_x[i][j] * err_0[i];
+        temp += train_x[i][j] * err_0[i];
+      theta[j] -= alpha * temp / n;
     }
 
     error = sqrt(error2);
     if (error > last_error)
       alpha /= 1.2;
-
-    for (int i = 0; i < p; i++)
-      theta[i] -= alpha * delta[i] / n;
 
   } while (it++ < cnt && fabs(error - last_error) > epsilon);
 }
@@ -239,6 +264,48 @@ void tink2(double *theta,int thread_num) {
     #pragma omp parallel for num_threads(thread_num)
     for (int i = 0; i < p; i++)
       theta[i] -= alpha * delta[i] / n;
+
+  } while (it++ < cnt && fabs(error - last_error) > epsilon);
+
+}
+
+void tink3(double *theta,int thread_num) {
+  double alpha = 0.01;
+  double err_0[n];
+
+  for (int i = 0; i < p; i++)
+    theta[i] = (rand() % 10) / 1000.0;
+
+  double delta[p] = {0};
+  double y_pred[n] = {0};
+  int cnt = 2000000, it = 0;
+  double last_error, error = 100;
+  do {
+    last_error = error;
+    int j;
+    double error2 = 0;
+
+    #pragma omp parallel for private(j) num_threads(thread_num) schedule(static,8) reduction(+:error2)
+    for (int i = 0; i < n; i++) {
+      y_pred[i] = 0;
+      for (j = 0; j < p; j++) {
+        y_pred[i] += theta[j] * train_x[i][j];
+      }
+      err_0[i] = y_pred[i] - train_y[i];
+      error2 += err_0[i] * err_0[i];
+    }
+
+    #pragma omp parallel for num_threads(thread_num) schedule(static,1)
+    for (int j = 0; j < p; j++){
+      double temp=0;
+      for(int i=0;i<n;i++)
+        temp += train_x[i][j] * err_0[i];
+      theta[j] -= alpha * temp / n;
+    }
+
+    error = sqrt(error2);
+    if (error > last_error)
+      alpha /= 1.2;
 
   } while (it++ < cnt && fabs(error - last_error) > epsilon);
 
